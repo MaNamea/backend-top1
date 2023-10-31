7097db0dbee86afc16384a263e0483ec3e74bbc7
MaNamea <146809339+MaNamea@users.noreply.github.com>
2023-10-31 15:30:47 +0300

    1

---
+++ timer/app.js
@@ -0,0 +1,37 @@
+let mode = 'full';
+const output = document.getElementById('output');
+const fullBtn = document.getElementById('full');
+const dateBtn = document.getElementById('date');
+const timeBtn = document.getElementById('time');
+function bindMode(ravage) {
+  
+  return function () {
+    mode = ravage;
+    update();
+  };
+}
+
+fullBtn.onclick = bindMode('full');
+dateBtn.onclick = bindMode('date');
+timeBtn.onclick = bindMode('time');
+update();
+
+setInterval(update, 1000);
+
+function update() {
+  output.textContent = format(mode);
+}
+
+function format(formatMode) {
+  const now = new Date();
+  switch (formatMode) {
+    case 'time':
+      return now.toLocaleTimeString();
+    case 'date':
+      return now.toLocaleDateString();
+    case 'full':
+      return now.toLocaleDateString() + ' ' + now.toLocaleTimeString();
+    default:
+      return now.toLocaleTimeString();
+  }
+}
